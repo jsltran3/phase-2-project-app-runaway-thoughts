cafdd37811d71fb63c70c73d6aa37e20f2a4c78d
Joseph <jsltran@gmail.com>
2022-06-22 14:49:58 -0700

    task category filters

--- my-app/src/JS/Tasks.js
+++ my-app/src/JS/Tasks.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
+import TasksFilter from "./TasksFilter";
+
 
 function Tasks() {
     const [tasks, setTasks] = useState([]); 
+    const [selectedCategory, setSelectedCategory] = useState("All");
 
     //fetch task list 
     useEffect(() => {
         fetch("http://localhost:3000/tasks")
             .then((resp) => resp.json())
-            .then((items) => console.log(items));
+            .then((tasks) => setTasks(tasks));
         }, []);
-    
+    const tasksToDisplay = tasks.filter((item => {
+        if (selectedCategory === "All") return true;
+
+        return item.category === selectedCategory;
+    }));
+
+    function handleCategoryChange() {
+        setSelectedCategory(selectedCategory)
+    }
 
     console.log("Component rendering");
 
@@ -17,13 +28,16 @@ function Tasks() {
         <div>
             <p>I am tasks!</p>
             <form>
-            <label>
-                Name:
-                <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Submit" />
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <input type="submit" value="Submit" />
             </form>
-
+            <TasksFilter 
+                category={selectedCategory}
+                onCategoryChange={handleCategoryChange}
+            />
             <h3>Task Card</h3>
             <footer>Footer: Maybe instructions at the bottom or something</footer>
         </div>
